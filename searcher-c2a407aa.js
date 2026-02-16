8e38da6954321ee3faf06deba52305a8540c420e
Wybxc <Wybxc@users.noreply.github.com>
2026-02-16 02:24:34 +0000

    deploy: 00422234e29db39d5b028c3f2178876abc146981

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b1950972.js',
+                path_to_root + 'searchindex-0940be3d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
